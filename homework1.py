6235431eca1a5147e89d85c077e606c24e9e7980
brunofares <faresbruno04@gmail.com>
2025-02-28 02:24:02 +0200

    Cleaned up the solve function. Removed the unnecessary functions for each different algorithm.

--- homework1.py
+++ homework1.py
@@ -112,6 +112,10 @@ class PuzzleSolver:
 
     # Main Solve function
     def solve(self):
+        self.setStates()
+        self.eliminateIllegalStates()
+        self.buildGraph()
+
         if (self.algorithm == "BFS"):
             self.solveBFS()
         elif (self.algorithm == "DFS"):
@@ -123,54 +127,18 @@ class PuzzleSolver:
 
     # BFS
     def solveBFS(self):
-        if (self.characters == 2):
-            self.solveBFS2()
-        elif (self.characters == 3):
-            self.solveBFS3()
-
-    def solveBFSwith2Chars(self):
-        pass
-
-    def solveBFSwith3Chars(self):
         pass
 
     # DFS
     def solveDFS(self):
-        if (self.characters == 2):
-            self.solveDFSwith2Chars()
-        elif (self.characters == 3):
-            self.solveDFSwith3Chars()
-
-    def solveDFSwith2Chars(self):
-        pass
-
-    def solveDFSwith3Chars(self):
         pass
 
     # DLS
     def solveDLS(self):
-        if (self.characters == 2):
-            self.solveDLSwith2Chars()
-        elif (self.characters == 3):
-            self.solveDLSwith3Chars()
-
-    def solveDLSwith2Chars(self):
-        pass
-
-    def solveDLSwith3Chars(self):
         pass
 
     # IDDFS
     def solveIDDFS(self):
-        if (self.characters == 2):
-            self.solveIDDFSwith2Chars()
-        elif (self.characters == 3):
-            self.solveIDDFSwith3Chars()
-
-    def solveIDDFSwith2Chars(self):
-        pass
-
-    def solveIDDFSwith3Chars(self):
         pass
 
     # Print solution
@@ -178,7 +146,5 @@ class PuzzleSolver:
         pass
 
 puzzleSolver = PuzzleSolver(3, "BFS")
-puzzleSolver.setStates()
-puzzleSolver.eliminateIllegalStates()
-puzzleSolver.buildGraph()
+puzzleSolver.solve()
 print(puzzleSolver.states)
